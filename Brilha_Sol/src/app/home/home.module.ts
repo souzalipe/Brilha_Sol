34379b7f9deac370343c746ca230e9ea878ad679
brenddamachado <brenda16machado.bm@gmail.com>
2024-08-18 13:14:43 -0300

    add: adicionadno o header na home

--- Brilha_Sol/src/app/home/home.module.ts
+++ Brilha_Sol/src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { HeaderComponent } from '../header/header.component';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, HeaderComponent]
 })
 export class HomePageModule {}
