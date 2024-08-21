5676a1916982fbfbb87f475727f1c3c9f1aeb352
brenddamachado <brenda16machado.bm@gmail.com>
2024-08-21 19:14:02 -0300

    fix: home finalizada

--- Brilha_Sol/src/app/splash/splash.page.ts
+++ Brilha_Sol/src/app/splash/splash.page.ts
@@ -11,7 +11,10 @@ export class SplashPage implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
-
+    setTimeout(() => {
+      
+      this.router.navigateByUrl('/home');
+    }, 5000);
   }
 
 }
