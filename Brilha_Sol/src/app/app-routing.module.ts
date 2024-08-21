4cda740fa9312d90e59642449f81ed1025317598
brenddamachado <brenda16machado.bm@gmail.com>
2024-08-21 19:02:47 -0300

    fix: atualizando as rotas

--- Brilha_Sol/src/app/app-routing.module.ts
+++ Brilha_Sol/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'splash',
+    loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
+  },
 ];
 
 @NgModule({
