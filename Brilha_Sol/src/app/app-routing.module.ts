a6e04a0fe6a09a78a3f59d3b63ad1ae9d2298890
Felipe Souza <fn23886@gmail.com>
2024-08-22 21:36:20 -0300

    add: login

--- Brilha_Sol/src/app/app-routing.module.ts
+++ Brilha_Sol/src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'splash',
     loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+
 ];
 
 @NgModule({
