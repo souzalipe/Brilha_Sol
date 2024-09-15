11d1e646423bb64eef9edbb27fcd9139a4e6d85c
brenddamachado <brenda16machado.bm@gmail.com>
2024-09-15 18:56:31 -0300

    fix: corrigindo cards

--- Brilha_Sol/src/app/home/home.page.ts
+++ Brilha_Sol/src/app/home/home.page.ts
@@ -8,13 +8,14 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   plants = [
-    { name: 'Plant 1', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/filme fino.webp' },
+    { name: 'Plant 1', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/placa.png' },
+    { name: 'Plant 2', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/placa2.png' },
+    { name: 'Plant 3', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/placa3.png' },
+    { name: 'Plant 4', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/placa4.png' },
+    { name: 'Plant 1', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/placa5.png' },
     { name: 'Plant 2', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/manocristalino.jpg' },
     { name: 'Plant 3', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/policristalino.png' },
-    { name: 'Plant 1', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/filme fino.webp' },
-    { name: 'Plant 2', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/manocristalino.jpg' },
-    { name: 'Plant 3', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/policristalino.png' },
-  
+    { name: 'Plant 4', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/maps.webp' }
   ];
 
   constructor() {}
