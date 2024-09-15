9ace8aeb7b4c8b38565aeca2d4f0940055ba4351
brenddamachado <brenda16machado.bm@gmail.com>
2024-09-15 18:13:43 -0300

    feat: adicionando mais  cards

--- Brilha_Sol/src/app/home/home.page.ts
+++ Brilha_Sol/src/app/home/home.page.ts
@@ -11,7 +11,10 @@ export class HomePage {
     { name: 'Plant 1', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/filme fino.webp' },
     { name: 'Plant 2', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/manocristalino.jpg' },
     { name: 'Plant 3', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/policristalino.png' },
-    { name: 'Plant 4', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/maps.webp' }
+    { name: 'Plant 1', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/filme fino.webp' },
+    { name: 'Plant 2', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/manocristalino.jpg' },
+    { name: 'Plant 3', location: 'Kazhakootam', status: 'Active', capacity: '1300 (kWp)', image: '../../assets/policristalino.png' },
+  
   ];
 
   constructor() {}
