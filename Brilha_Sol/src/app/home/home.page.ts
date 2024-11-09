8be23a7bc4990f3d4e9223d5a5baa4ab3a6758d8
Felipe Souza <fn23886@gmail.com>
2024-11-09 09:49:24 -0300

    feat : mercado pago and home

--- Brilha_Sol/src/app/home/home.page.ts
+++ Brilha_Sol/src/app/home/home.page.ts
@@ -8,6 +8,7 @@ interface Product {
   name: string;
   valor: string;
   image: string;
+  link: string;
 }
 
 @Component({
@@ -17,14 +18,14 @@ interface Product {
 })
 export class HomePage {
   plants: Product[] = [
-    { name: 'Placa Solar Painel Solar 150w + Manual', valor: "375,10", image: '../../assets/placa.png' },
-    { name: 'Kit Painel Placa Energia Solar 2x155w Caminhão 12ou24 Volts Cor Azul Voltagem De Circuito Aberto 24.46v', valor: "875,00", image: '../../assets/placa2.png' },
-    { name: 'Kit Sistema Solar Painel Led A Bateria + 3 Lampadas Portatil', valor: "175,75", image: '../../assets/placa3.png' },
-    { name: 'Painel solar portátil Ecoflow 110W', valor: "1.123,30", image: '../../assets/placa4.png' },
-    { name: 'Painel Solar Ezviz D 7.2v 4,2w Para Câmera Externa A Bateria', valor: "195,70", image: '../../assets/placa5.png' },
-    { name: 'String Box Clamper 4 Entradas / 4 Saídas', valor: "2.094,00", image: '../../assets/placa6.webp' },
-    { name: 'Estação Solar Portátil Autônoma 3 Lâmpadas E Carregamento', valor: "259,90", image: '../../assets/placa7.webp' },
-    { name: 'Painel Solar 12v 20w Policristalino Fotovoltaico (placa)', valor: "149,99", image: '../../assets/placa8.webp' }
+    { name: 'Placa Solar Painel Solar 150w + Manual', valor: "375,10", image: '../../assets/placa.png', link:"https://mpago.la/28P14uN" },
+    { name: 'Kit Painel Placa Energia Solar 2x155w Caminhão 12ou24 Volts Cor Azul Voltagem De Circuito Aberto 24.46v', valor: "875,00", image: '../../assets/placa2.png',link:"https://mpago.la/28P14uN" },
+    { name: 'Kit Sistema Solar Painel Led A Bateria + 3 Lampadas Portatil', valor: "175,50", image: '../../assets/placa3.png',link:"https://mpago.la/28P14uN" },
+    { name: 'Painel solar portátil Ecoflow 110W', valor: "1.123,30", image: '../../assets/placa4.png',link:"https://mpago.la/28P14uN"},
+    { name: 'Painel Solar Ezviz D 7.2v 4,2w Para Câmera Externa A Bateria', valor: "195,70", image: '../../assets/placa5.png',link:"https://mpago.la/28P14uN" },
+    { name: 'String Box Clamper 4 Entradas / 4 Saídas', valor: "2.094,00", image: '../../assets/placa6.webp',link:"https://mpago.la/28P14uN" },
+    { name: 'Estação Solar Portátil Autônoma 3 Lâmpadas E Carregamento', valor: "259,90", image: '../../assets/placa7.webp',link: "https://mpago.la/28P14uN"},
+    { name: 'Painel Solar 12v 20w Policristalino Fotovoltaico (placa)', valor: "149,99", image: '../../assets/placa8.webp', link:"https://mpago.la/28P14uN"}
   ];
 
   constructor(
